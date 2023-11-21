007c659301ed76cf5c1da8fbb886f66b37de7934
suryakumara <suryakumara33@gmail.com>
2023-11-21 13:42:42 +0800

    add news page

--- resources/views/livewire/component/front/carousel.blade.php
+++ resources/views/livewire/component/front/carousel.blade.php
@@ -2,62 +2,66 @@
     <section id="carousel" class="carousel relative shadow-2xl bg-white ">
         <div class="carousel-inner relative overflow-hidden w-full ">
             <!--Slide-->
-            @for ($item = count($carousels)-1; $item >= 0; $item--)
+            @for ($item = count($carousels) - 1; $item >= 0; $item--)
                 @php $index = ($item + 1) @endphp
                 <input class="hidden carousel-open" type="radio" id="carousel-{{ $index }}" name="carousel"
                     aria-hidden="true" hidden="" checked="checked">
                 <div class="carousel-item absolute opacity-0 overflow-hidden">
-                    @if($carousels[$item]['bgvideo'])
-                    <div class=" relative bg-bg-dark text-white ">
-                        <video class="absolute object-cover w-full h-full scale-125" autoplay loop muted>
-                            <source src="{{ Storage::disk('carousel')->url($carousels[$item]['bgvideo']) }}" type="video/mp4">
-                        </video>
-                        <div class="p-20 z-30 flex-col justify-center items-center flex" data-aos="fade-up"
-                            data-aos-duration="3000">
-                            <p class="custom-shadow font-bold mt-6 md:mt-0 text-2xl md:text-5xl font-poppins">
-                                {{$carousels[$item]['heading_sm']}}</p>
-                            <p class="custom-shadow text-4xl md:text-6xl md:mb-4 md:mt-4 text-yellow-300 font-lobster">
-                                {{$carousels[$item]['heading_lg']}}
-                            </p>
-                            <p class="custom-shadow text-xl md:text-4xl mt-6 md:mt-0">
-                                {{$carousels[$item]['heading_md']}}
-                            </p>
+                    @if ($carousels[$item]['bgvideo'])
+                        <div class=" relative bg-bg-dark text-white ">
+                            <video class="absolute object-cover w-full h-full scale-125" autoplay loop muted>
+                                <source src="{{ Storage::disk('carousel')->url($carousels[$item]['bgvideo']) }}"
+                                    type="video/mp4">
+                            </video>
+                            <div class="p-20 z-30 flex-col justify-center items-center flex" data-aos="fade-up"
+                                data-aos-duration="3000">
+                                <p class="custom-shadow font-bold mt-6 md:mt-0 text-2xl md:text-5xl font-poppins">
+                                    {{ $carousels[$item]['heading_sm'] }}</p>
+                                <p
+                                    class="custom-shadow text-4xl md:text-6xl md:mb-4 md:mt-4 text-yellow-300 font-lobster">
+                                    {{ $carousels[$item]['heading_lg'] }}
+                                </p>
+                                <p class="custom-shadow text-xl md:text-4xl mt-6 md:mt-0">
+                                    {{ $carousels[$item]['heading_md'] }}
+                                </p>
+                            </div>
                         </div>
-                    </div>
                     @else
-                    <div class="grid grid-rows-2 md:grid-cols-2 text-white text-5xl text-center ">
-                        <div class=" flex-col p-6 md:justify-center items-center flex" data-aos="fade-up"
-                            data-aos-duration="3000">
-                            <p class="mt-6 md:mt-0 text-3xl font-poppins">{{ $carousels[$item]['heading_sm'] }}</p>
-                            <p class="text-4xl md:text-5xl md:mb-4 md:mt-4 text-yellow-300 font-lobster">
-                                {{ $carousels[$item]['heading_lg'] }}
-                            </p>
-                            <p class="text-xl md:text-4xl mt-6 md:mt-0">{{ $carousels[$item]['heading_md'] }}</p>
-                            <p class="hidden md:block mt-20 text-lg font-poppins">{{ $carousels[$item]['description'] }}</p>
-                            <ul
-                                class=" flex-wrap md:mt-20 space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400 text-lg hidden md:flex items-center h-10 gap-5 ">
-                                @foreach ($carousels[$item]['sections'] ? json_decode($carousels[$item]['sections'], true) : [] as $section)
-                                    <li class="text-gray-200 hover:text-blue-500 transition cursor-pointer text-base md:text-xl list-none"
-                                        data-aos="fade-up" data-aos-duration="1000"
-                                        data-aos-anchor-placement="top-bottom">
-                                        <svg class="text-blue-500 inline w-3 h-3 dark:text-white" aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 10 16">
-                                            <path
-                                                d="M3.414 1A2 2 0 0 0 0 2.414v11.172A2 2 0 0 0 3.414 15L9 9.414a2 2 0 0 0 0-2.828L3.414 1Z" />
-                                        </svg>
-                                        <a href="javascript:void(0)"
-                                            data-target="{{ $section['link_section'] }}">{{ Formatting::getLang() == 'id' ? $section['label'] : $section['label_en'] }}</a>
-                                    </li>
-                                @endforeach
-                            </ul>
+                        <div class="grid grid-rows-2 md:grid-cols-2 text-white text-5xl text-center ">
+                            <div class=" flex-col p-6 md:justify-center items-center flex" data-aos="fade-up"
+                                data-aos-duration="3000">
+                                <p class="mt-6 md:mt-0 text-3xl font-poppins">{{ $carousels[$item]['heading_sm'] }}</p>
+                                <p class="text-4xl md:text-5xl md:mb-4 md:mt-4 text-yellow-300 font-lobster">
+                                    {{ $carousels[$item]['heading_lg'] }}
+                                </p>
+                                <p class="text-xl md:text-4xl mt-6 md:mt-0">{{ $carousels[$item]['heading_md'] }}</p>
+                                <p class="hidden md:block mt-20 text-lg font-poppins">
+                                    {{ $carousels[$item]['description'] }}</p>
+                                <ul
+                                    class=" flex-wrap md:mt-20 space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400 text-lg hidden md:flex items-center h-10 gap-5 ">
+                                    @foreach ($carousels[$item]['sections'] ? json_decode($carousels[$item]['sections'], true) : [] as $section)
+                                        <li class="text-gray-200 hover:text-blue-500 transition cursor-pointer text-base md:text-xl list-none"
+                                            data-aos="fade-up" data-aos-duration="1000"
+                                            data-aos-anchor-placement="top-bottom">
+                                            <svg class="text-blue-500 inline w-3 h-3 dark:text-white" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                viewBox="0 0 10 16">
+                                                <path
+                                                    d="M3.414 1A2 2 0 0 0 0 2.414v11.172A2 2 0 0 0 3.414 15L9 9.414a2 2 0 0 0 0-2.828L3.414 1Z" />
+                                            </svg>
+                                            <a href="javascript:void(0)"
+                                                data-target="{{ $section['link_section'] }}">{{ Formatting::getLang() == 'id' ? $section['label'] : $section['label_en'] }}</a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <div class="">
+                                <img src="{{ Storage::disk('carousel')->url($carousels[$item]['bgimage']) }}"
+                                    alt="" srcset="" class="h-full object-cover object-center">
+                            </div>
                         </div>
-                        <div class="">
-                            <img src="{{ Storage::disk('carousel')->url($carousels[$item]['bgimage']) }}" alt=""
-                                srcset="" class="h-full object-cover object-center">
-                        </div>
-                    </div>
                     @endif
-                    <a @click="scrollToDiv('search')" class="justify-center items-center flex cursor-pointer">
+                    <a @click="scrollToDiv('info')" class="justify-center items-center flex cursor-pointer">
                         <svg class="absolute text-white hover:text-gray-900 duration-300 bottom-0 w-10 h-10 animate-bounce"
                             viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
@@ -94,7 +98,7 @@
 
             <!-- Add additional indicators for each slide-->
             <ol class="carousel-indicators">
-                @for ($item = count($carousels)-1; $item >= 0; $item--)
+                @for ($item = count($carousels) - 1; $item >= 0; $item--)
                     @php $index = ($item + 1) @endphp
                     <li class="inline-block mr-3">
                         <label for="carousel-{{ $index }}"
@@ -119,11 +123,11 @@
                         const nextIndex = (currentIndex + 1) % radioCount;
                         const nextRadio = radios[nextIndex];
 
-                        
+
                         currentRadio.checked = false;
                         nextRadio.checked = true;
-                        
-                        
+
+
                         currentRadio = nextRadio;
                     }, 5000);
                 }
--- resources/views/livewire/component/front/news.blade.php
+++ resources/views/livewire/component/front/news.blade.php
@@ -1,64 +1,87 @@
 @extends('layouts.base')
 @section('body')
-<div>
-    <section class="text-gray-600 body-font">
-        <div class="container px-5 py-24 mx-auto max-w-7x1">
-            <div class="flex flex-wrap w-full mb-4 p-4">
-                <div class="w-full mb-6 lg:mb-0">
-                    <h1 class="sm:text-4xl text-5xl font-bold title-font mb-2 text-gray-900">Berita</h1>
-                    <div class="h-1 w-20 bg-indigo-500 rounded"></div>
+    <div class=" min-h-screen">
+        <style>
+            .container-sub {
+                margin-top: 112px;
+            }
+
+            .container-news {
+                max-height: 550px;
+            }
+        </style>
+        <div class="flex flex-col min-h-screen bg-white  dark:bg-gray-700">
+            @livewire('component.front.navbar')
+            <div class="container-sub text-gray-500 p-5 pb-5 text-center grid grid-cols-3 gap-4">
+                <div class="col-span-2">
+                    <div class="h-full p-5 flex flex-col rounded-lg shadow bg-white dark:bg-gray-800">
+
+                        <h1 class=" text-2xl text-gray-900 dark:text-white font-bold">
+                            Penyerahan Ijin Program Studi Program Profesi Insinyur (PS.PPI) Universitas Warmadewa
+                        </h1>
+                        <span class="font-bold text-base text-gray-500 truncate dark:text-gray-400 mt-3">
+                            4 hari yang lalu
+                        </span>
+
+                        <figure class="card-zoom-news">
+                            <img class="card-zoom-image w-full rounded-lg over:scale-110 transition duration-500 object-cover"
+                                src="{{ asset('img/logo-merdeka.png') }}" alt="">
+                        </figure>
+                        <p class=" text-justify mt-3">Lorem ipsum, dolor sit amet consectetur
+                            adipisicing elit. Commodi,
+                            ducimus voluptate. Iste
+                            consequatur accusantium hic repellendus laboriosam. Illum saepe soluta minus aspernatur,
+                            dicta vel quibusdam totam consectetur distinctio inventore. Esse veniam, facere quae sunt
+                            laborum sapiente fugit in itaque, possimus explicabo repellat temporibus cum. Nulla quisquam
+                            expedita quam reiciendis quaerat ex voluptate, accusamus aperiam illum unde doloremque
+                            consequuntur. Dolor corrupti rerum adipisci eaque ex necessitatibus, tenetur quo tempora
+                            omnis numquam ipsam, autem labore aut? Rem quisquam, accusamus a libero illo fuga error in
+                            doloremque nam eum maiores, repellat excepturi. Reiciendis officia laudantium illum neque
+                            placeat fuga repellat dignissimos totam, doloribus eveniet at quae magnam ducimus
+                            perspiciatis eum harum. Consequuntur autem fuga maiores cumque. Sint cupiditate officia
+                            ipsam voluptate optio? Reprehenderit similique, corporis magni unde minima et possimus, quae
+                            laboriosam porro dolore ullam quam esse? Omnis odio reprehenderit, natus laudantium porro
+                            necessitatibus, dignissimos ea facere quibusdam minus dolores voluptates atque corrupti
+                            fugit veniam a in architecto minima repudiandae laboriosam asperiores placeat explicabo?
+                            Illum laborum ratione eligendi dicta praesentium, explicabo sapiente quidem? Aliquam
+                            suscipit tempora cum quia optio libero voluptate ratione magni blanditiis nemo, quis eum
+                            necessitatibus quos aliquid? Sit asperiores, cumque quia excepturi molestias commodi
+                            explicabo, nam error molestiae veritatis ea repellendus sunt possimus, harum natus nemo
+                            reprehenderit nostrum eveniet dolorum! Laboriosam corrupti possimus tenetur numquam
+                            distinctio. Nihil culpa aliquid repellendus quod aliquam officia, tempore modi similique
+                            architecto perferendis placeat labore mollitia pariatur esse voluptatem! Tenetur rerum magni
+                            provident sit, commodi temporibus ut a neque consequatur. Praesentium reprehenderit est
+                            aliquam quia nostrum totam? Quaerat nostrum pariatur, illum odio neque provident cupiditate
+                            dolorem error animi excepturi blanditiis doloribus ipsam nihil. Voluptate culpa iusto nemo
+                            omnis rerum modi doloribus, est ipsa repellat ullam sed facilis voluptatem alias molestiae
+                            reprehenderit officiis necessitatibus, odit ratione tenetur deserunt. Dignissimos quidem
+                            nisi quasi deleniti aperiam eveniet, asperiores magni aliquid omnis necessitatibus
+                            repudiandae voluptates ab nostrum doloremque magnam culpa quam voluptatum neque dicta, ad
+                            illo in architecto quos. Velit mollitia laborum odio sequi iste debitis odit voluptate
+                            perspiciatis ipsa totam placeat quidem, voluptatem deserunt. Cum facere hic corrupti alias
+                            consequuntur tempora quasi nam aut adipisci officiis error repudiandae obcaecati magnam sunt
+                            saepe harum at dolore facilis, optio vitae, quisquam omnis, ea doloribus. Dicta a vero
+                            voluptatibus modi qui placeat animi labore repellendus, ipsum veritatis fugiat quaerat
+                            temporibus tenetur deserunt, aliquam, nihil inventore reprehenderit hic illum? Dolor
+                            reiciendis nesciunt enim dolorum. Sequi neque sint eaque impedit, dolorum praesentium
+                            temporibus nulla facere sit autem. Itaque illo et voluptas! Enim aliquid repellendus ea
+                            similique veniam omnis velit consectetur. Iure architecto excepturi tempora dolorem eius
+                            commodi. Porro soluta sapiente a, quo voluptates consequuntur officiis dolores ea
+                            blanditiis, eligendi dolor ab? Eaque quis commodi tempora fuga ullam dolor aperiam soluta
+                            saepe eos placeat, porro sint dolores ex cumque, sunt nesciunt sapiente sed nulla nemo quia
+                            aliquam vitae voluptates numquam. Odio voluptatem alias aliquam! Illo itaque ea autem
+                            aperiam asperiores? Cumque saepe eligendi itaque corporis, voluptates sunt architecto
+                            laboriosam ipsam eveniet cum quis, iusto impedit qui. Ut, iusto omnis fugit culpa
+                            exercitationem tempore inventore?</p>
+
+                    </div>
+
+                </div>
+                <div class="container-news">
+                    @livewire('component.front.news.news-update')
                 </div>
             </div>
-            <div class="flex flex-wrap -m-4">
-                <div class="xl:w-1/3 md:w-1/2 p-4">
-                    <div class="bg-white p-6 rounded-lg">
-                        <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72  rounded w-full object-cover object-center mb-6"
-                            src="https://kuyou.id/content/images/ctc_2020021605150668915.jpg" alt="Image Size 720x400">
-                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
-                        <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat.
-                            Distillery
-                            hexagon disrupt edison bulbche.</p>
-                    </div>
-                </div>
-                <div class="xl:w-1/3 md:w-1/2 p-4">
-                    <div class="bg-white p-6 rounded-lg">
-                        <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72 rounded w-full object-cover object-center mb-6"
-                            src="https://asset.kompas.com/crops/Pk_pN6vllxXy1RshYsEv74Q1BYA=/56x0:1553x998/750x500/data/photo/2021/06/16/60c8f9d68ff4a.jpg"
-                            alt="Image Size 720x400">
-                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
-                        <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat.
-                            Distillery
-                            hexagon disrupt edison bulbche.</p>
-                    </div>
-                </div>
-                <div class="xl:w-1/3 md:w-1/2 p-4">
-                    <div class="bg-white p-6 rounded-lg">
-                        <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72 rounded w-full object-cover object-center mb-6"
-                            src="https://images.immediate.co.uk/production/volatile/sites/7/2019/07/33-GettyImages-154260931-216706f.jpg?quality=90&resize=768%2C574"
-                            alt="Image Size 720x400">
-                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Great Pyramid of Giza</h2>
-                        <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat.
-                            Distillery
-                            hexagon disrupt edison bulbche.</p>
-                    </div>
-                </div>
-                <div class="xl:w-1/3 md:w-1/2 p-4">
-                    <div class="bg-white p-6 rounded-lg">
-                        <img class="lg:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72 rounded w-full object-cover object-center mb-6"
-                            src="https://wisatamuda.com/wp-content/uploads/2019/02/1-Golden-Gate-Bridge-Gambar-dan-Foto-Tempat-Wisata-Terbaik-di-San-Fransisco-USA.jpg"
-                            alt="Image Size 720x400">
-                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">San Francisco</h2>
-                        <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat.
-                            Distillery
-                            hexagon disrupt edison bulbche.</p>
-                    </div>
-                </div>
-            </div>
+            @livewire('component.front.footer.footer-base')
         </div>
-    </section>
-</div>
+    </div>
 @endsection
